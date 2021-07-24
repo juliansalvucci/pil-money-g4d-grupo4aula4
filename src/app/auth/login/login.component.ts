5ffb20562a2e8b01c68fe100ac2251005bbddc9f
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-07-24 09:35:34 -0300

    Material Module

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -7,38 +7,6 @@ import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  hide = true;
-
-  
-  /*
-  loginForm: FormGroup;
-
-  error: boolean = false;
-  mensajeError: string = "";
-  
-  
-  
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      'usuario': ['', Validators.required],
-      'contraseña': ['', Validators.required]
-    });
-  }
-
-  login():void {
-    if (this.loginForm.invalid) {
-      this.error = true;
-      this.mensajeError = "Campo requerido"
-    }else{
-      this.error = false;
-      
-    }
-
-
-  }
-  */
-  
-
   
 
 }
