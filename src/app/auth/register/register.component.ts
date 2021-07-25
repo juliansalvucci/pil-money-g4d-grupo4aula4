353ee311986b05e98cd6170dcb18ab16d81ccf79
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-07-25 20:47:49 -0300

    ramaparaYunior

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,29 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}
-  }]
+  providers: []
 })
-export class RegisterComponent implements OnInit {
-
-  firstFormGroup!: FormGroup;
-  secondFormGroup!: FormGroup;
-
-  constructor(private _formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-  }
+export class RegisterComponent {
 
 }
