5db78a5d221d403e942e027a9e8ff391cefa3bc0
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-08-16 22:07:03 -0300

    Modulo Transferencia

---
+++ src/app/src/app/transferencia/transferencia.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TransferenciaRoutingModule } from './transferencia-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TransferenciaRoutingModule
+  ]
+})
+export class TransferenciaModule { }
