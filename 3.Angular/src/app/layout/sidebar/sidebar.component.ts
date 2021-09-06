21c9822c11ef4fe78515bde0f29c00b968fc82d9
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-09-06 08:05:20 -0300

    Merge manual Pons-Ariel (SideBarComponent)

--- 3.Angular/src/app/layout/sidebar/sidebar.component.ts
+++ 3.Angular/src/app/layout/sidebar/sidebar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  constructor() { }
+  constructor(private observer: BreakpointObserver) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
   }
-
 }
