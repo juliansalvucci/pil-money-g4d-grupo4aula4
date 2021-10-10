1005c648cae64e2bc85efa2810023c5b69176f48
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-10-09 21:23:16 -0300

    Listado de cuentas pantalla principal

--- 3.Angular/src/app/Modulos/cuenta/cuenta.module.ts
+++ 3.Angular/src/app/Modulos/cuenta/cuenta.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CuentaComponent } from './cuenta/cuenta.component';
 import { MaterialModule } from 'src/app/Material/material.module';
-import { CuentaCreateComponent } from './cuenta-create/cuenta-create.component';
 
 
 
@@ -10,7 +9,6 @@ import { CuentaCreateComponent } from './cuenta-create/cuenta-create.component';
 @NgModule({
   declarations: [
     CuentaComponent,
-    CuentaCreateComponent
   ],
   imports: [
     CommonModule,
--- 3.Angular/src/app/Modulos/cuenta/cuenta/cuenta.component.ts
+++ 3.Angular/src/app/Modulos/cuenta/cuenta/cuenta.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Cuenta } from 'src/app/Interfaces/Cuenta';
+import { CuentaService } from 'src/app/Servicios/cuenta.service';
+import { TipoMonedaService } from 'src/app/Servicios/tipo-moneda.service';
 
 @Component({
   selector: 'app-cuenta',
   templateUrl: './cuenta.component.html',
   styleUrls: ['./cuenta.component.css']
 })
-export class CuentaComponent implements OnInit {
+export class CuentaComponent {
 
-  constructor() { }
+  listaCuentas!: Cuenta[];
 
-  ngOnInit(): void {
+  constructor(private cuentaService: CuentaService, private tipoMonedaService: TipoMonedaService){
+      this.getCuentas();
   }
+
+  getCuentas(){
+    this.cuentaService.obtenerCuentas().subscribe(r => {
+      console.log(r);
+      this.listaCuentas = r;
+    })
+  }
+
+  getTipoMonedas(){}
 
 }
