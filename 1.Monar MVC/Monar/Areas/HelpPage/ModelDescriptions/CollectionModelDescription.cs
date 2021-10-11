7ece66a72a31ba86cb6d6eac5b7381ea6dc3e26b
Yuniord1991 <55953871+Yuniord1991@users.noreply.github.com>
2021-10-11 17:57:25 -0300

    Merge branch 'main' into RodriguezYunior

--- 1.Monar MVC/Monar/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Monar.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
